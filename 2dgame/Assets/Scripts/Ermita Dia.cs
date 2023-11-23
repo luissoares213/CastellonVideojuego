fa05ae1c76c9c81962d54bac1c057648fe9e98fb
SantiDieste <al415652@uji.es>
2023-11-23 17:50:04 +0100

    Mapa y Camara

---
+++ 2dgame/Assets/Scripts/Ermita Dia.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ErmitaDia : MonoBehaviour
+{
+
+
+    public Transform newCamPosition;
+
+    public void MoveCameraXY()
+    {
+        if (newCamPosition != null)
+        {
+            Vector3 newCamPositionXY = new Vector3(newCamPosition.position.x, newCamPosition.position.y, Camera.main.transform.position.z);
+            Camera.main.transform.position = newCamPositionXY;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
