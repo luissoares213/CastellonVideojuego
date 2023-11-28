61752f3ebb0009f54e1d2098dd595af3fbef8d50
AngelLopezOrtiz <al405262@uji.es>
2023-11-28 20:46:03 +0100

    arreglo

--- 2dgame/Assets/Scripts/MovimientoPersonaje.cs
+++ 2dgame/Assets/Scripts/MovimientoPersonaje.cs
@@ -42,11 +42,11 @@ public class MovimientoPersonaje : MonoBehaviour
             transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
 
             // Verificar si el personaje ha llegado a la posición objetivo
-<<<<<<< Updated upstream
+//Updated upstream
             if (Vector2.Distance(transform.position, targetPosition) < 10f)
-=======
+
             if (Vector2.Distance(transform.position, targetPosition) < 5f)
->>>>>>> Stashed changes
+//Stashed changes
             {
                 isMoving = false;
                 animator.SetBool("Andar", false);
