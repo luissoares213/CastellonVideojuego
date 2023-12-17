7035bc1d919023473c7dbd0b1f7c25d5ef3e0427
AlexCS04 <alexcanosala@gmail.com>
2023-12-17 15:28:25 +0100

    SFX controller

--- 2dgame/Assets/Scripts/ControlMusica.cs
+++ 2dgame/Assets/Scripts/ControlMusica.cs
@@ -6,9 +6,10 @@ using UnityEngine.Audio;
 public class ControlMusica : MonoBehaviour
 {
     [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private string nameSound;
 
     public void VolMus (float sliderMus)
     {
-        audioMixer.SetFloat("VolumenMus", Mathf.Log10(sliderMus)*20);
+        audioMixer.SetFloat(nameSound, Mathf.Log10(sliderMus)*20);
     }
 }
