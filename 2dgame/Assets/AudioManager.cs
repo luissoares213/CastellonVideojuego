1099d81b3e27eb61e255dc0d7582f9cab1738bf0
AlexCS04 <90848343+AlexCS04@users.noreply.github.com>
2024-01-29 22:22:29 +0100

    Musica

--- 2dgame/Assets/AudioManager.cs
+++ 2dgame/Assets/AudioManager.cs
@@ -27,24 +27,30 @@ public class AudioManager : MonoBehaviour
     void Update()
     {
         string actual = SceneManager.GetActiveScene().name;
-        //if (actual == "" || actual == "" || actual == "") {
-        //    musicSource.clip = music4;
-        //    musicSource.
-        //}
-        if (actual == "MenuInicial") {
-            musicSource.clip = music1;
-        }
-        else if (actual=="AngelMinijuego")
+        if (actual == "Escena2-MuseoNoche" || actual == "Escena6-MuseoNoche2" || actual == "Epilogo-Museo")
         {
             musicSource.clip = music4;
-        }
-        else if (actual == "JoelMinijuego")
-        {
-            musicSource.clip = music3;
+            musicSource.pitch = 0.45f;
         }
         else
         {
-            musicSource.clip = music2;
+            musicSource.pitch = 1f;
+            if (actual == "MenuInicial")
+            {
+                musicSource.clip = music1;
+            }
+            else if (actual == "AngelMinijuego")
+            {
+                musicSource.clip = music4;
+            }
+            else if (actual == "JoelMinijuego")
+            {
+                musicSource.clip = music3;
+            }
+            else
+            {
+                musicSource.clip = music2;
+            }
         }
         if (!musicSource.isPlaying) {
             musicSource.Play();
